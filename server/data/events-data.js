5808f4cf1803ca9fb83cdd133c090801e7747b8c
Lingeswaran <linges01032gmail.com>
2022-06-10 10:07:10 +0530

    API

---
+++ server/data/events-data.js
@@ -0,0 +1,43 @@
+var events = [
+  {
+    id: "1",
+    company: "Amazon",
+    Experience: "12",
+    Age: "22",
+    "Year of Passing": "2022",
+    Location: "Bangalore",
+    Skill: "React",
+  },
+  {
+    id: "2",
+    company: "Microsoft",
+    Experience: "3",
+    Age: "Between 19 to 28",
+    Technology: "Cloud",
+    Skill: "Angular",
+    Location: "Bangalore",
+    Language: "English,Spanish",
+  },
+  {
+    id: "3",
+    company: "IBM",
+    Experience: "12",
+    Age: "More than 29",
+    Role: "Senior Software Developer",
+    Location: "USA",
+    Language: "English (Highly Recomended)",
+    Technology: "Spring",
+    LPA: "56 (Include House Allowance)",
+    "Seat Available": "128",
+  },
+  {
+    id: "4",
+    company: "cool",
+    Experience: "6",
+    Age: "23",
+    Skill: "React",
+    Location: "Bangalore",
+  },
+];
+
+module.exports = events;
